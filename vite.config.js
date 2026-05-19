50a230ab5e94877ad7f68cb0e90daf7b14a0fecb
ArthurVieira-eng <arthurv.o.2507@gmail.com>
2026-05-19 01:42:14 -0300

    fix: ajuste de base path absoluto e regras de rewrite do vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 Mude para './' para funcionar em qualquer lugar!
+  base: '/', // caminho absoluto da raiz
 })
